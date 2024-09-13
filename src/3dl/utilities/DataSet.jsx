294e938fd12e2b9cb8680898e86bfea1f33853f3
jamiearodi <arodichacha@gmail.com>
2024-09-13 22:52:22 +0300

    Cleaned up dataset

--- src/3dl/utilities/DataSet.jsx
+++ src/3dl/utilities/DataSet.jsx
@@ -11,23 +11,17 @@ const DataSet = ({
   useQuery,
   children,
   dataConnection,
+  pageSize = 15,
 }) => {
-  //Balancing the search and the pagination fetch logic
+  // Balancing the search and the pagination fetch logic
   // On search, we can reset the index and offset, so that we are fetching fresh data that has been filtered
 
   const [paginatedData, setPaginatedData] = useState([]);
-  const pageSize = 15; // Fixed page size //to be a prop defined in the 3dl
-  const [infiniteScrollConfig, setInfiniteScrollConfig] = useState({
-    pageIndex: 1,
-  });
 
-  // Calculate the offset based on the pageIndex
-  const offset = (infiniteScrollConfig.pageIndex - 1) * pageSize;
+  const { offset, nextPage } = useInfiniteScroll(pageSize);
 
-  // Replace placeholders in the query with actual values //add options for other other databases
-  const paginatedQuery = query
-    .replace("{pageSize}", pageSize)
-    .replace("{offset}", offset);
+  // Replace placeholders in the query with actual values
+  const paginatedQuery = buildPaginatedQuery(query, pageSize, offset);
 
   // Pass the modified query to useDataSetLogic
   const { data, loading, error, state } = useDataSetLogic(
@@ -42,25 +36,19 @@ const DataSet = ({
       setPaginatedData((paginatedData) => [...paginatedData, ...data]);
   }, [data]);
 
-  //this was disabled to allow aesthetic for the infinite scroll behaviour, should resolve
+  // this was disabled to allow aesthetic for the infinite scroll behaviour, should resolve
   // if (loading) {
   //   return <div>Loading data...</div>;
   // }
 
-  const updaterFunction = () => {
-    setInfiniteScrollConfig((prevConfig) => ({
-      ...prevConfig,
-      pageIndex: prevConfig?.pageIndex + 1,
-    }));
-  };
-
-  if (paginatedQuery.includes("OFFSET"))
-    if (error) {
-      return <div>Error fetching data: {error.message}</div>;
-    }
+  if (error) {
+    return <div>Error fetching data: {error.message}</div>;
+  }
 
   return (
-    <DataContext.Provider value={{ data: paginatedData, updaterFunction }}>
+    <DataContext.Provider
+      value={{ data: paginatedData, updaterFunction: nextPage }}
+    >
       {/* Conditionally render Debug On if debug is true */}
       {state.debug && (
         <div style={{ color: "red", fontWeight: "bold" }}>Debug On</div>
@@ -68,6 +56,29 @@ const DataSet = ({
       {children}
     </DataContext.Provider>
   );
+};
+
+const buildPaginatedQuery = (query, pageSize, offset) => {
+  return query.replace("{pageSize}", pageSize).replace("{offset}", offset);
+};
+
+const useInfiniteScroll = (pageSize) => {
+  const [infiniteScrollConfig, setInfiniteScrollConfig] = useState({
+    pageIndex: 1,
+  });
+
+  // Calculate the offset based on the pageIndex and pageSize
+  const offset = (infiniteScrollConfig.pageIndex - 1) * pageSize;
+
+  // Function to update the page index
+  const nextPage = () => {
+    setInfiniteScrollConfig((prevConfig) => ({
+      ...prevConfig,
+      pageIndex: prevConfig?.pageIndex + 1,
+    }));
+  };
+
+  return { pageIndex: infiniteScrollConfig.pageIndex, offset, nextPage };
 };
 
 export default DataSet;
