c615c262b7c3b3c529f0ca0572a6c8c8797e12de
GoodluckMlungusye <goodluckmlungusye@gmail.com>
2024-09-17 11:46:36 +0300

    Add default pivot rows and columns

--- src/3dl/tables/PivotTable.jsx
+++ src/3dl/tables/PivotTable.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PivotTableUI from "react-pivottable/PivotTableUI";
 import "react-pivottable/pivottable.css";
 import { useDashboardContext } from "../utilities/Dashboard";
@@ -9,24 +9,61 @@ const PivotTable = ({
   header = "Pivot Table",
   subHeader = header,
   variant = "card", // Default to "card" variant
+  pivotRows = [],
+  pivotCols = []
 }) => {
-  const [pivotState, setPivotState] = useState({});
+
+  const initialPivotRows = pivotRows;
+  const initialPivotCols = pivotCols;
+
+  const [pivotState, setPivotState] = useState({
+    data: [],
+    rows: initialPivotRows,
+    cols: initialPivotCols
+  });
+
   const { state } = useDashboardContext();
   const { data } = useDataContext();
 
   // Defensive check for data
   const hasValidData = data && Array.isArray(data) && data.length > 0;
 
-  // Content to be rendered inside the ChartComponent
+  useEffect(() => {
+    if (hasValidData) {
+      // Get the keys of the first object in data
+      const keys = Object.keys(data[0]);
+
+      // Use provided pivotRows or default to the first key
+      const activePivotRows = initialPivotRows.length > 0 ? initialPivotRows : [keys[0]];
+      const activePivotCols = initialPivotCols.length > 0 ? initialPivotCols : keys.slice(1, 6);
+
+      // Only update the state if the new rows or cols differ from the current state
+      setPivotState((prevState) => {
+        if (
+          prevState.data !== data ||
+          JSON.stringify(prevState.rows) !== JSON.stringify(activePivotRows) ||
+          JSON.stringify(prevState.cols) !== JSON.stringify(activePivotCols)
+        ) {
+          return {
+            ...prevState,
+            data: data,
+            rows: activePivotRows,
+            cols: activePivotCols,
+          };
+        }
+        return prevState;
+      });
+    }
+  }, [data, initialPivotRows, initialPivotCols]);
+
   const content = hasValidData ? (
     <div>
       {state.debug && (
         <div style={{ color: "red", fontWeight: "bold" }}>Debug On</div>
       )}
       <PivotTableUI
-        data={data}
-        onChange={(s) => setPivotState(s)}
         {...pivotState}
+        onChange={newState => setPivotState(newState)}
       />
     </div>
   ) : (
