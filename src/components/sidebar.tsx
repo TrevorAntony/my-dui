afa208fe66cc9640af6e65b55195bda4ccaad319
GoodluckMlungusye <goodluckmlungusye@gmail.com>
2024-10-17 12:58:35 +0300

    restore deleted code

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -22,9 +22,8 @@ import DuftModal from "./duft-modal";
 import { renderModalContent } from "../helpers/modalContentHelper";
 
 const ExampleSidebar: FC = function () {
-  const {
-    isOpenOnSmallScreens: isSidebarOpenOnSmallScreens,
-  } = useSidebarContext();
+  const { isOpenOnSmallScreens: isSidebarOpenOnSmallScreens } =
+    useSidebarContext();
 
   const [currentPage, setCurrentPage] = useState("");
 
@@ -211,6 +210,7 @@ const BottomMenu: FC = function () {
     setIsModalOpen(false);
     setModalContent([]);
     channel.postMessage({ type: "TOGGLE_MODAL", isModalOpen: false });
+    window.location.href = "/dashboard/home";
   };
 
   const divStyle = {
