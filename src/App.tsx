4359956a7e1c7c03acb21a8268adada88f9e9232
jamiearodi <arodichacha@gmail.com>
2024-11-28 12:21:59 +0300

    Deprecate auth provider

--- src/App.tsx
+++ src/App.tsx
@@ -16,36 +16,33 @@ import SingleTableLayoutTester from "./content-components/SingleTableLayoutTeste
 import DataTaskHandler from "./ui-components/data-task-handler";
 import Settings from "./ui-components/duft-settings/duft-settings";
 import Login from "./ui-components/login";
-import { AuthProvider } from "./context/AuthContext";
 import "./App.css";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<AppLayout />}>
-            {/* Root path renders Dashboard3DL with 'home' as the ID param */}
-            <Route index element={<Dashboard3DL defaultId="home" />} />
-            <Route path="dashboard/:id?" element={<Dashboard3DL />} />
-            <Route path="a/:id?" element={<ComponentA />} />
-            <Route path="b" element={<ComponentB />} />
-            <Route path="c" element={<ComponentC />} />
-            <Route path="d" element={<ComponentD />} />
-            <Route path="grid" element={<GridLayoutTester />} />
-            <Route path="tab" element={<TabLayoutTester />} />
-            <Route path="table" element={<SingleTableLayoutTester />} />
-            <Route path="data-task/:id" element={<DataTaskHandler />} />
-            <Route path="settings" element={<Settings />} />
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          {/* Root path renders Dashboard3DL with 'home' as the ID param */}
+          <Route index element={<Dashboard3DL defaultId="home" />} />
+          <Route path="dashboard/:id?" element={<Dashboard3DL />} />
+          <Route path="a/:id?" element={<ComponentA />} />
+          <Route path="b" element={<ComponentB />} />
+          <Route path="c" element={<ComponentC />} />
+          <Route path="d" element={<ComponentD />} />
+          <Route path="grid" element={<GridLayoutTester />} />
+          <Route path="tab" element={<TabLayoutTester />} />
+          <Route path="table" element={<SingleTableLayoutTester />} />
+          <Route path="data-task/:id" element={<DataTaskHandler />} />
+          <Route path="settings" element={<Settings />} />
 
-            {/* Catch-all route for unavailable routes */}
-            <Route path="*" element={<Navigate to="/dashboard/home" />} />
-          </Route>
+          {/* Catch-all route for unavailable routes */}
+          <Route path="*" element={<Navigate to="/dashboard/home" />} />
+        </Route>
 
-          {/* Public routes */}
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </AuthProvider>
+        {/* Public routes */}
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </Router>
   );
 };
