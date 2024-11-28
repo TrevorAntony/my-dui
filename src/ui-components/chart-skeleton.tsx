0e0a39417bf44edd06c166d41114f188ec910f81
Trevor Antony <trvromondi@gmail.com>
2024-11-28 13:46:07 +0300

    Skeleton cleanup and ThemeMode Optimization

--- src/ui-components/chart-skeleton.tsx
+++ src/ui-components/chart-skeleton.tsx
@@ -1,10 +1,18 @@
+import React from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 const ChartSkeleton = () => {
+  const isDarkMode = localStorage.getItem("flowbite-theme-mode") === "dark";
+
   return (
     <div>
-      <Skeleton height={300} width="100%" />
+      <Skeleton
+        height={300}
+        width="100%"
+        baseColor={isDarkMode ? "#4B5563" : "#F9FAFB"}
+        highlightColor={isDarkMode ? "#6B7280" : "#F3F4F6"}
+      />
     </div>
   );
 };
