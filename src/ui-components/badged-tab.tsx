9f1aa3a4185565b399a08e76bedde034489d8d38
GoodluckMlungusye <goodluckmlungusye@gmail.com>
2024-12-30 10:10:04 +0300

    Create re-usable badged duft tab

--- src/ui-components/badged-tab.tsx
+++ src/ui-components/badged-tab.tsx
@@ -1,8 +1,50 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 import { Tabs, Badge } from "flowbite-react";
 import { HiAdjustments, HiClipboardList, HiUserCircle } from "react-icons/hi";
 import { MdDashboard } from "react-icons/md";
 import { DuftTabset } from "./tab-components";
+import type { IconType } from "react-icons";
+
+interface BadgedDuftTabProps {
+  active?: boolean;
+  title: string;
+  badgeContent?: string | number;
+  icon?: IconType;
+  children: ReactNode;
+}
+
+export const BadgedDuftTab: FC<BadgedDuftTabProps> = ({
+  active = false,
+  title,
+  badgeContent,
+  icon: Icon,
+  children,
+}) => {
+  return (
+    <Tabs.Item
+      active={active}
+      title={
+        <div className="flex items-center gap-2">
+          <span>{title}</span>
+          {badgeContent && <Badge color="info">{badgeContent}</Badge>}
+        </div>
+      }
+      icon={Icon}
+    >
+      {children}
+    </Tabs.Item>
+  );
+};
+
+// sample usage
+
+{
+  /* <BadgedDuftTab active title="Profile" badgeContent="50" icon={HiUserCircle}>
+  <span className="font-medium p-4 text-gray-800 dark:text-white">
+    Profile tab's associated content
+  </span>
+</BadgedDuftTab> */
+}
 
 export const BadgedTab: FC = () => {
   return (
