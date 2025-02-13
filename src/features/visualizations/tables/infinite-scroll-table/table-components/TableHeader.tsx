55ed7ffdb580033f6786c3ce90409ba7f5372fc1
Trevor Antony <trvromondi@gmail.com>
2025-02-13 09:04:38 +0300

    Remove default arrows from table

--- src/features/visualizations/tables/infinite-scroll-table/table-components/TableHeader.tsx
+++ src/features/visualizations/tables/infinite-scroll-table/table-components/TableHeader.tsx
@@ -54,20 +54,16 @@ const TableHeader: React.FC<TableHeaderProps> = ({
                   {" "}
                   {transformCase(header, caseType)}
                 </span>
-                <svg
-                  className={`ml-1.5 h5 w-5 flex-shrink-0 ${
-                    sortState[header]
-                      ? "text-highlight-600 dark:text-highlight-600"
-                      : "text-gray-600 dark:text-gray-300"
-                  }`}
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  {sortState[header] ? (
+                {sortState[header] && (
+                  <svg
+                    className="ml-1.5 h5 w-5 flex-shrink-0 text-highlight-600 dark:text-highlight-600"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
                     <path
                       stroke="currentColor"
                       strokeLinecap="round"
@@ -79,16 +75,8 @@ const TableHeader: React.FC<TableHeaderProps> = ({
                           : "M8 10v10m0-10-3 3m3-3 3 3" // Down
                       }
                     />
-                  ) : (
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M8 20V10m0 10-3-3m3 3 3-3m5-13v10m0-10 3 3m-3-3-3 3"
-                    />
-                  )}
-                </svg>
+                  </svg>
+                )}
               </div>
             </th>
           ))}
